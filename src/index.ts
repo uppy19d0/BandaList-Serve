e19c9ec53f871708989202e4aa758347c61e4925
Luis A.Tavarez <luis_aneuris16@hotmail.com>
2021-05-09 20:20:32 -0400

    change to project with typescript

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 import * as dotenv from 'dotenv';
 import express = require('express');
 import path = require('path');
+var favicon = require('serve-favicon')
 // import express  from 'express';
 
 dotenv.config();
@@ -23,6 +24,7 @@ const publicPath = path.resolve(__dirname, 'public')
 //     res.sendFile(path.resolve(__dirname,"./public/index.html"));
 //   });
 app.use(express.static(publicPath));
+app.use(favicon(__dirname + '/dist/favicon.ico'));
 server.listen(process.env.PORT, (error) => {
     if(error) throw new Error(error);
     console.log('Serve Run in port:', process.env.PORT)
--- src/tools/copyAssets.ts
+++ src/tools/copyAssets.ts
@@ -1,7 +1,7 @@
 import * as shell from "shelljs";
 
 // Copy all the view templates and assets in the public folder
-shell.cp("-R", ["src/public"], "dist/");
+shell.cp("-R", ["src/public","src/favicon.ico"], "dist/");
 
 // // Remove unnecessary files
 // shell.rm(["dist/public/js/*.ts", "dist/public/js/*.json"]);
