2b16889e9875374e3bb85c4ebceff27e710f1b45
Luis A.Tavarez <luis_aneuris16@hotmail.com>
2021-05-09 20:37:07 -0400

    change to project with typescript

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 const express = require("express");
 const path = require("path");
+// var favicon = require('serve-favicon')
 // import express  from 'express';
 dotenv.config();
 // const express = require('express');
@@ -16,11 +17,12 @@ const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
 require('./sockets/socket');
 //Path publico
-// const publicPath = path.resolve(__dirname, 'public')
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./public/index.html"));
-});
-// app.use(express.static(publicPath));
+const publicPath = path.resolve(__dirname, 'public');
+// app.get("/", (req: any, res: any) => {
+//     res.sendFile(path.resolve(__dirname,"./public/index.html"));
+//   });
+app.use(express.static(publicPath));
+// app.use(favicon(__dirname + 'favicon.ico'));
 server.listen(process.env.PORT, (error) => {
     if (error)
         throw new Error(error);
--- dist/tools/copyAssets.js
+++ dist/tools/copyAssets.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const shell = require("shelljs");
 // Copy all the view templates and assets in the public folder
-shell.cp("-R", ["src/public"], "dist/");
+shell.cp("-R", ["src/public", "src/favicon.ico"], "dist/");
 // // Remove unnecessary files
 // shell.rm(["dist/public/js/*.ts", "dist/public/js/*.json"]);
 //# sourceMappingURL=copyAssets.js.map
